c2a0cd352cacb405c2ff87e36a8acc15acd0c654
Aleaume <aliaume64@gmail.com>
2021-10-19 21:54:29 +0200

    update score.py

--- Project-Solution-Cousseau/score.py
+++ Project-Solution-Cousseau/score.py
@@ -10,11 +10,22 @@ import json
 
 def init():
     print("This is init")
-
+    model_path = Model.get_model_path("./")
+    print("Model Path is:",model_path)
+    model = joblib.load(model_path)
 
 def run(data):
-    test = json.loads(data)
-    print(f"received data {test}")
+    data = json.loads(data)
+    result = model.predict(data['data'])
+    return {'data' : result.tolist() , 'message' : 'Successfully 
+            predicted'}
+   except Exception as e:
+      error = str(e)
+      return {'data' : error , 'message' : 'Failed to predict 
+             '}
+    print(f"received data {data}")
     return f"test is {test}"
 
 #SOURCE / HELP :https://docs.microsoft.com/en-us/azure/machine-learning/how-to-deploy-and-where?tabs=python
+
+#https://medium.com/daily-programming-tips/how-to-deploy-a-local-ml-model-as-a-web-service-on-azure-machine-learning-studio-5eb788a2884c
