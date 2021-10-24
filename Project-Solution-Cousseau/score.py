cc70f20893c913b5576e5a6c68b7920d8533e50c
Aleaume <aliaume64@gmail.com>
2021-10-24 19:03:55 +0200

    changes but unsuccessful with score.py

--- Project-Solution-Cousseau/score.py
+++ Project-Solution-Cousseau/score.py
@@ -6,11 +6,24 @@
 #make sure file "score.py" is placed in the directory : "./source_dir"
 
 import json
+import joblib
+import pickle
+import numpy as np
+import pandas as pd
+
+import azureml.automl.core
+from azureml.automl.core.shared import logging_utilities, log_server
+from azureml.core.model import Model
+
+
 
 
 def init():
+    global Model
     print("This is init")
-    model_path = Model.get_model_path("./")
+
+    model_path = Model.get_model_path("AleaumeModel")
+    #model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'),'model.pkl')
     print("Model Path is:",model_path)
     model = joblib.load(model_path)
 
@@ -19,12 +32,10 @@ def run(data):
         data = json.loads(data)
         result = model.predict(data['data'])
         return {'data' : result.tolist() , 'message' : 'Successfully predicted'}
-   except Exception as e:
+    except Exception as e:
       error = str(e)
       return {'data' : error , 'message' : 'Failed to predict '}
-    print(f"received data {data}")
-    return f"test is {test}"
-
+    
 #SOURCE / HELP :https://docs.microsoft.com/en-us/azure/machine-learning/how-to-deploy-and-where?tabs=python
 
 #https://medium.com/daily-programming-tips/how-to-deploy-a-local-ml-model-as-a-web-service-on-azure-machine-learning-studio-5eb788a2884c
